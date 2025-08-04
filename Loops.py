82c71e9bad52b0ee05dde124857df4a2b02d3619
siddhik92 <siddhikawade92@gmail.com>
2025-08-04 23:49:40 +0530

    sole some while loop que

--- Loops.py
+++ Loops.py
@@ -4,9 +4,24 @@ while(count<=5):
     count += 1
     print("Hello")
 
-count=1
+# count=1
 while count<=5 :
     print("hello")
     count += 1
 print(count)
 
+
+#print no from 1 to 5
+i=0
+while i<=5:
+    i+=1
+    print(i)
+
+print("end")
+
+#5 to 1
+i=5
+while i>=1:
+    print(i)
+    i-=1
+print("end")
---
+++ QueWhileloop.py
@@ -0,0 +1,16 @@
+#print nos from 1 to 100
+i=1
+while i<=100:
+    print(i)
+    i+=1
+
+print("done")
+
+
+#print nos from 100 to 1
+i=100
+while i>=1:
+    print(i)
+    i-=1
+
+#print multiplication table of no n
