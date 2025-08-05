df6f2fbb9978a0d6ab703b3c88a9e06f1a948096
siddhik92 <siddhikawade92@gmail.com>
2025-08-05 07:30:14 +0530

    learn break,continue,for loops

--- Loops.py
+++ Loops.py
@@ -1,10 +1,10 @@
-#while Loop
+#1while Loop
 count=1
 while(count<=5):
     count += 1
     print("Hello")
 
-# count=1
+# # count=1
 while count<=5 :
     print("hello")
     count += 1
@@ -25,3 +25,58 @@ while i>=1:
     print(i)
     i-=1
 print("end")
+
+
+#2.break
+i=1
+while i<=5:
+    print(i)
+    if(i==3):
+        break
+    i +=1
+print("end of loop")
+
+nums=[1,4,9,16,25,36,49,64,81,100]
+x=36
+i=0
+while i<len(nums):
+    if (nums[i]==x):
+        print("found at idx",i)
+        break
+    i += 1
+
+#3 continue
+i=0
+while i<=5:
+    if(i==3):
+        i+=1
+        continue  #skip
+    print(i) 
+    i +=1   
+
+# print odd no
+i=1
+while i<=10:
+    if(i%2==0):
+        i+=1
+        continue
+    print(i)
+    i+=1
+
+#for Loop
+nums=[1,2,3,4,5]
+for val in nums:
+    print(val)
+
+veg=["potato","brijal","ladyfinger","cucumber"]
+for val in veg:
+    print(val)     
+
+tup=(1,2,3,4,5)
+for num in tup:
+    print(num)
+
+str='siddhi'
+for char in str:
+    print(char)
+
