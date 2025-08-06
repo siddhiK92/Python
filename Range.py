acc90fd0a6b0c93c7ea485265cc7321a14451731
siddhik92 <siddhikawade92@gmail.com>
2025-08-06 07:23:31 +0530

    Learn range , loops,pass stmt

---
+++ Range.py
@@ -0,0 +1,40 @@
+#Range function
+# seq=range(5)
+# for i in seq:
+#     print(i)
+
+# for i in range(10):  ##range(stop)
+#     print (i) 
+
+
+# for i in range(2, 10):  #range(start, stop)
+#     print (i) 
+
+# for i in range(2, 10, 2):  ##range(stop)
+#     print (i) 
+
+# for i in range(101):
+#     print(i)
+
+#even no print
+# for i in range(2, 101, 2):
+#     print(i)
+
+
+#odd no print
+# for i in range(1, 101, 2):
+#     print(i)
+
+# Quetions 
+# print number from 1 to 100
+# for i in range( 1, 101,1):
+#     print(i)
+
+#print 100 to 1
+# for i in range(100, 0, -1):
+#     print(i)
+
+#print the multiplication table of a n
+n=int(input("Enter a no: "))
+for i in range(1,11):
+    print(n*i)
